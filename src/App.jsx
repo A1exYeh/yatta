ebacd8a64b26cbb558f5f494600c97b846f7cecf
Alex Yeh <111445339+AlexYessirski@users.noreply.github.com>
2024-12-02 15:32:34 -0800

    change css in app.jsx to test tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="card bg-red-200 rounded-xl">
+        <button className="bg-red-700 p-8 rounded-lg" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
